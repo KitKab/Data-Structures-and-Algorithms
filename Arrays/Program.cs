329a8f25e8433dfada1a94068b8ef57e6c24adc0
KitKab <49355885+KitKab@users.noreply.github.com>
2019-10-05 21:04:48 -0700

    Kth Smallest Element

---
+++ Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ass4_8_KthSmallestElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 7, 10, 4, 3, 20, 15 };
+            int k = 3;
+            
+            int temp = 0;
+       
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for(int j=i+1;j<arr.Length;j++)
+                {
+                    if(arr[i]>arr[j])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                    }
+
+                }
+
+            }
+            Console.Write("The output is :" + arr[k-1]);
+            Console.ReadLine();
+        }
+    }
+}
